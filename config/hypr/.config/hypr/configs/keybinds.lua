df2868a52991d4a2f35c98bda3313fb999b26ffd
luca <lucae.sdp@gmail.com>
2026-09-25 20:40:30 -0300

    feat(hyprland): add keybinds.lua

---
+++ config/hypr/.config/hypr/configs/keybinds.lua
@@ -0,0 +1,25 @@
+-- keybinds
+
+-- vars
+
+-- program vars
+local term = "kitty"
+local browser = "firefox --new-window about:profilemanager" 
+local filemngr = "kitty yazi"
+local 
+
+-- key vars
+local mainmod = "SUPER"
+
+-- main binds
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
+hl.bind(mainmod .. " + B", hl.dsp.exec_cmd(browser))
+hl.bind(mainmod .. " + E", hl.dsp.exec_cmd(filemngr))
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
+hl.bind(mainmod .. " + Q", hl.dsp.exec_cmd(term))
